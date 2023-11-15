1958f15aac3761c4c6ddadd656d6bebdb3d776b3
Aus10 <amaxj95@gmail.com>
2023-11-15 20:29:56 +0000

    release_build_1

--- outside-the-box.eth/src/App.js
+++ outside-the-box.eth/src/App.js
@@ -1,3 +1,5 @@
+//CHAT_API_KEY=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
+
 import React, { useState } from 'react';
 import logo from './outsidetheboxlogo.png'; // Ensure this path is correct
 import './App.css';
@@ -13,6 +15,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Welcome to Outside the Box!</h1><br></br>
+      <nav className="App-nav">
+        <nav className="App-nav">
+            <a href="https://junk-ez.net">Junk Hauling Services</a>
+            <a href="https://discord.gg/Q6hyCATE">The Discord</a>
+            <a href="https://github.com/amaxj95/outsidethebox.eth">The Source</a>
+            <a href="">Leave a Review!</a>
+          </nav>
+        </nav><br></br><br></br><br></br>
         <img 
           src={logo} 
           className="App-logo" 
@@ -20,7 +31,6 @@ function App() {
           onClick={toggleButtons} 
           style={{ cursor: 'pointer' }}
         />
-
       </header>
     </div>
   );
